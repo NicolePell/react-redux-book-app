de8501eebd1829e309091e56b4300d83f012881e
NicolePell <pellicena@gmail.com>
2016-12-07 09:16:22 +0000

    L42: implement reducers and container class for book list

---
+++ src/reducers/books_reducer.js
@@ -0,0 +1,14 @@
+export default function() {
+  return [
+    { title: "The Hitch Hiker's Guide to the Galaxy" },
+    { title: "The Restaurant at the End of the Universe" },
+    { title: "Life, The Universe and Everything" },
+    { title: "So Long and Thanks for All The Fish" },
+    { title: "Mostly Harmless" },
+    { title: "Dirk Gently's Holistic Detective Agency" },
+    { title: "The Long Dark Tea-Time of the Soul" },
+    { title: "Last Chance to See" },
+    { title: "The Meaning of Liff" },
+    { title: "The Deeper Meaning of Liff" }
+  ]
+}
