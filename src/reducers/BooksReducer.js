931d05c6f70a1fc80435da467930429b0e9b5411
nicpell <npellicenatredway@deloitte.co.uk>
2016-12-07 16:44:22 +0000

    L50: add action and book detail

---
+++ src/reducers/BooksReducer.js
@@ -0,0 +1,14 @@
+export default function() {
+  return [
+    { title: "The Hitch Hiker's Guide to the Galaxy", pages: 101 },
+    { title: "The Restaurant at the End of the Universe", pages: 20 },
+    { title: "Life, The Universe and Everything", pages: 600 },
+    { title: "So Long and Thanks for All The Fish", pages: 1 },
+    { title: "Mostly Harmless", pages: 30 },
+    { title: "Dirk Gently's Holistic Detective Agency", pages: 50 },
+    { title: "The Long Dark Tea-Time of the Soul", pages: 52 },
+    { title: "Last Chance to See", pages: 306 },
+    { title: "The Meaning of Liff", pages: 77 },
+    { title: "The Deeper Meaning of Liff", pages: 89 }
+  ]
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
-import BooksReducer from './books_reducer';
+import BooksReducer from './BooksReducer';
+import ActiveBookReducer from './ActiveBookReducer';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBookReducer
 });
 
 export default rootReducer;
